790918001913e51637e7b3b91b10a91005b849e8
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2024-10-07 12:09:30 +0100

    chore: fix favicon

--- src/routes/contact/InputField.svelte
+++ src/routes/contact/InputField.svelte
@@ -48,7 +48,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<!-- Favicon -->
-	<link rel="icon" href="/favicon.ico" />
+	<!-- <link rel="icon" href="/favicon.ico" /> -->
 </svelte:head>
 
 <div class="field">
